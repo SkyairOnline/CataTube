7fc2d18618460036643c0467dfb02db2933bd6a6
Aldo Surya Ongko <skyaironline@gmail.com>
2020-10-22 15:41:13 +0700

    Add splash screen
    Signed-off-by: Aldo Surya Ongko <skyaironline@gmail.com>

---
+++ app/src/main/java/com/arudo/catatube/ui/splash/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.arudo.catatube.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import com.arudo.catatube.R
+import com.arudo.catatube.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        if (supportActionBar != null) {
+            (supportActionBar as ActionBar).hide()
+        }
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
